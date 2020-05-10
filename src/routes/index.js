1ce88914cc8a2966c1432be7a4e0453c28e38f12
1arp <dekhijaegi@gmail.com>
2020-05-11 03:02:39 +0530

    changed callback url

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const SpotifyWebApi = require('spotify-web-api-node');
 var spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  redirectUri: 'http://localhost:8080/callback'
+  redirectUri: 'https://spotifyliked.herokuapp.com/callback'
 });
 
 route.get('/',(req, res)=>{
